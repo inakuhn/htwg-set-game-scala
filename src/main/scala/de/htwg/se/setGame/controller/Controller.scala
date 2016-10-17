e600856105cbe60cea74f51d23faa1981b5d1a0f
rabertol <raina.bertolini@gmail.com>
2016-10-17 16:50:07 +0200

    #4 add controller Trait and Object for implementation

---
+++ src/main/scala/de/htwg/se/setGame/controller/Controller.scala
@@ -0,0 +1,8 @@
+package de.htwg.se.setGame.controller
+
+/**
+ * Created by raina on 17.10.2016.
+ */
+trait Controller {
+  def startGame : Unit
+}
---
+++ src/main/scala/de/htwg/se/setGame/controller/imp/ControllerImp.scala
@@ -0,0 +1,10 @@
+package de.htwg.se.setGame.controller.imp
+
+import de.htwg.se.setGame.controller.Controller
+
+/**
+ * Created by raina on 17.10.2016.
+ */
+object ControllerImp extends Controller{
+  override def startGame: Unit = ???
+}
