a0e9dd747cd00e5c1a04946431e3f7ed5be6feed
Philipp Daniels <philipp.daniels@gmail.com>
2016-12-02 00:04:47 +0100

    #4 Initiate Tui and Controller on application start

--- src/main/scala/de/htwg/se/setGame/SetGameApplication.scala
+++ src/main/scala/de/htwg/se/setGame/SetGameApplication.scala
@@ -1,26 +1,8 @@
 package de.htwg.se.setGame
 
-import com.typesafe.scalalogging.Logger
-import de.htwg.se.setGame.model.ModelFactory
-import de.htwg.se.setGame.model.imp.CardAttribute.Color._
-import de.htwg.se.setGame.model.imp.CardAttribute.Fill._
-import de.htwg.se.setGame.model.imp.CardAttribute.Form._
-import de.htwg.se.setGame.model.imp.CardAttribute.Count._
-
 object SetGameApplication {
-  private val logger = Logger(getClass)
-
   def main(args: Array[String]): Unit = {
-    val card1 = ModelFactory.card(wave, green, empty, one)
-    val card2 = ModelFactory.card(ellipse, red, halfFilled, two)
-    val card3 = ModelFactory.card(balk, purple, filled, tree)
-    val playerOne = ModelFactory.player(0, 0, "Joe")
-    val playerTwo = ModelFactory.player(1, 0, "Doe")
-    val players = List(playerOne, playerTwo)
-    val unusedCards = List(card1, card2)
-    val cardsInField = List(card3)
-    val session = ModelFactory.session(players, unusedCards, cardsInField)
-
-    logger.info(session.toString)
+    val controller = Controller()
+    Tui(controller)
   }
 }
