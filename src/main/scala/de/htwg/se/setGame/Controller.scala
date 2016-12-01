fc2638a429be4255fe92063475fdbbd0a91b4c9f
Philipp Daniels <philipp.daniels@gmail.com>
2016-12-01 22:41:51 +0100

    Add controller with factory

---
+++ src/main/scala/de/htwg/se/setGame/Controller.scala
@@ -0,0 +1,15 @@
+package de.htwg.se.setGame
+
+/**
+  * @author Philipp Daniels
+  */
+protected class Controller {
+
+}
+
+/**
+  * @author Philipp Daniels
+  */
+object Controller {
+  def apply(): Controller = new Controller()
+}
