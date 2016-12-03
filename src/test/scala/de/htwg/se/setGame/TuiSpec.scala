decf4b1f0f45e32e7e9fd65f6f2556ff016d0105
Ina Kuhn <rabertol@htwg-konstanz.de>
2016-12-03 16:17:15 +0100

    test Console.with(in)

--- src/test/scala/de/htwg/se/setGame/TuiSpec.scala
+++ src/test/scala/de/htwg/se/setGame/TuiSpec.scala
@@ -1,8 +1,11 @@
 package de.htwg.se.setGame
 
+import java.io.ByteArrayInputStream
+
 import org.apache.log4j.Logger
 import org.scalatest.Matchers._
 import org.scalatest.WordSpec
+
 import scala.collection.mutable.ListBuffer
 import scala.swing.Reactions.Reaction
 
@@ -10,31 +13,34 @@ import scala.swing.Reactions.Reaction
   * @author Philipp Daniels
   */
 class TuiSpec extends WordSpec {
+  val in = new ByteArrayInputStream("x".getBytes)
   "Tui" should {
-    val listenerList = new ListBuffer[Reaction]()
+    scala.Console.withIn(in) {
+      val listenerList = new ListBuffer[Reaction]()
 
-    class ControllerSpy extends Controller {
-      override def subscribe(listener: Reaction): Unit = {
-        listenerList += listener
+      class ControllerSpy extends Controller {
+        override def subscribe(listener: Reaction): Unit = {
+          listenerList += listener
+        }
       }
-    }
 
-    val testAppender = new TestAppender
-    Logger.getRootLogger.removeAllAppenders()
-    Logger.getRootLogger.addAppender(testAppender)
+      val testAppender = new TestAppender
+      Logger.getRootLogger.removeAllAppenders()
+      Logger.getRootLogger.addAppender(testAppender)
 
-    val controller = new ControllerSpy
-    val target = Tui(controller)
+      val controller = new ControllerSpy
+      val target = Tui(controller)
 
-    "called listenTo" in {
-      listenerList should have length 2
-      listenerList contains target
-      listenerList contains controller
-    }
+      "called listenTo" in {
+        listenerList should have length 2
+        listenerList contains target
+        listenerList contains controller
+      }
 
-    "has initiate message" in {
-      testAppender.getLog().length should be >0
-      testAppender.getLog should include (Tui.InitiateMessage)
+      "has initiate message" in {
+        testAppender.getLog().length should be > 0
+        testAppender.getLog should include(Tui.InitiateMessage)
+      }
     }
   }
 }
