7d5fe4763c9bc32f430150f3eef8ee3284c96517
Philipp Daniels <philipp.daniels@gmail.com>
2017-01-11 14:35:50 +0100

    #11 Add test for CardAttribute

---
+++ src/test/scala/de/htwg/se/setGame/model/CardAttributeSpec.scala
@@ -0,0 +1,13 @@
+package de.htwg.se.setGame.model
+
+import org.scalatest.Matchers._
+import org.scalatest.WordSpec
+
+class CardAttributeSpec extends WordSpec {
+
+  "CardAttribute" should {
+    "have a return" in {
+      val target = CardAttribute
+    }
+  }
+}
