43ec68f16ac509221a8d672f81597501eae6e541
Philipp Daniels <philipp.daniels@gmail.com>
2017-01-19 01:03:41 +0100

    Add working example for async actor test

--- src/test/scala/de/htwg/se/setGame/actor/CardActorSpec.scala
+++ src/test/scala/de/htwg/se/setGame/actor/CardActorSpec.scala
@@ -1,34 +1,34 @@
 package de.htwg.se.setGame.actor
 
-import java.util.concurrent.CountDownLatch
-
 import akka.actor.{ActorSystem, Props}
-import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec, WordSpecLike}
-import akka.testkit.{ImplicitSender, TestActorRef, TestActors, TestKit}
-import org.scalatest.Matchers._
-import akka.pattern.ask
-import de.htwg.se.setGame.model.Card
-import akka.util.Timeout
+import akka.testkit.{DefaultTimeout, ImplicitSender, TestKit}
 import com.typesafe.config.ConfigFactory
+import de.htwg.se.setGame.model.Card
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
-import scala.concurrent.Await
-import scala.util.Success
 import scala.concurrent.duration._
+import scala.language.postfixOps
 
 /**
   * Created by Ina Kuhn on 18.01.2017.
   */
-class CardActorSpec extends WordSpec {
-  private implicit val timeout = Timeout(5 seconds)
-  "CardActorSpec" should {
+class CardActorSpec extends TestKit(ActorSystem("CardActorSpec", ConfigFactory.parseString("akka {}")))
+  with DefaultTimeout
+  with ImplicitSender
+  with WordSpecLike
+  with Matchers
+  with BeforeAndAfterAll {
 
-    "generateCards" ignore   {
-      implicit val system = ActorSystem("TestSys")
-      val actorRef = TestActorRef.create(system, Props[CardActor])
-      // hypothetical message stimulating a '42' answer
-      val future = actorRef ? CreatePack
-      val result = Await.result(actorRef ? CreatePack, 5 seconds).asInstanceOf[List[Card]]
-      result.size should be(81)
+  "CardActorSpec" should {
+    val target = system.actorOf(Props[CardActor])
+    "generateCards" in {
+      within(500 millis) {
+        target ! CreatePack
+
+        receiveWhile(500 millis) {
+          case result: List[Card] => result.size should be(81)
+        }
+      }
 
     }
 
